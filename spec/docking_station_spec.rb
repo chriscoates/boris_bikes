412ee2c8860e8347082cc127a849a4bbb66c8047
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-12 13:11:01 +0200

    chapter 10, completed with grace

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,13 @@
 load 'docking_station.rb'
 
 
-describe DockingStation do
+  describe DockingStation do
+    it { is_expected.to respond_to :release_bike }
 
-it { is_expected.to respond_to :release_bike }
+  it "adds a new bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+
+  end
 
 end
