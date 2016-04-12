1a5d72a7053940e5b251a384599964324cce9ae1
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-12 18:20:12 +0200

    ch12 best coding exp

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,12 +4,22 @@ class DockingStation
 
 	attr_accessor :bike
 
+	def initialize
+		@bike_count = 1
+	end
+
 	def release_bike
-		Bike.new
+		if @bike_count == 0
+			fail  "No bikes to release"
+		else
+			@bike_count -= 1
+			Bike.new
+		end
 	end
 
 	def dock(bike)
 		@bike = bike
+		@bike_count += 1
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,15 +6,19 @@ load 'bike.rb'
 
   it "adds a new bike" do
     bike = subject.release_bike
-    expect(bike).to be_working
+    expect(bike).to be_working 
   end
 
   it "docks a bike at docking station" do
     bike = subject.release_bike
     subject.dock(bike)
-    expect(subject.bike).to eq bike 
+    expect(subject.bike).to eq bike
   end
 
+  it "raises error when there are no bikes left and there is a request to release bike" do
+    bike1 = subject.release_bike
+    expect {bike2 = subject.release_bike}.to raise_error("No bikes to release")
+  end
 
 
 
